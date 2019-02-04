e1fb5ff3d9bf84d736279537695a110005b70993
pratap47 <agrawal.akash407@gmail.com>
2019-02-04 22:29:31 +0530

    project made

---
+++ app/src/main/java/com/bullseye/bidding/login/authentication.java
@@ -0,0 +1,15 @@
+package com.bullseye.bidding.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bullseye.bidding.R;
+
+public class authentication extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authentication);
+    }
+}
